a16e0d711b0fa99acaf34505c6959f1fc2c1079f
Jamie Iong <jamwiong@gmail.com>
2015-07-02 11:45:50 -0600

    Create a Spring Repository

---
+++ src/main/java/com/jamwiong/spring/sample/JamwiongSpringSampleApplication.java
@@ -0,0 +1,12 @@
+package com.jamwiong.spring.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JamwiongSpringSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JamwiongSpringSampleApplication.class, args);
+    }
+}
