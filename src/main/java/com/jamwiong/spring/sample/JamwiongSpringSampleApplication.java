eb0db3d225302e484332c6ec10d83530deede247
Jamie Iong <jamwiong@gmail.com>
2015-07-02 11:50:01 -0600

    Add READ.txt and modify JamwiongSpringSampleApplication.java

--- src/main/java/com/jamwiong/spring/sample/JamwiongSpringSampleApplication.java
+++ src/main/java/com/jamwiong/spring/sample/JamwiongSpringSampleApplication.java
@@ -9,4 +9,5 @@ public class JamwiongSpringSampleApplication {
     public static void main(String[] args) {
         SpringApplication.run(JamwiongSpringSampleApplication.class, args);
     }
+    
 }
